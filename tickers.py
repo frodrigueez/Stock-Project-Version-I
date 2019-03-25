982bf8d007f0e7f8c987f815f8eda877c2e9a498
Guillermo Villegas <44106664+gevillegas@users.noreply.github.com>
2019-03-25 12:36:06 -0400

    Add files via upload

--- tickers.py
+++ tickers.py
@@ -4,17 +4,21 @@
 """
 
 import requests
-
+import re
 link = "http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQrender=download"
 
-f = requests.get(link)
-print(f.text)
+d = requests.get(link)
+#print(d.text
+html_file = open('html.txt', 'w')
+html_file.write(d.text)
+html_file = open('html.txt')
 
+# print(html_file)
 tickerbaselink = "https://www.nasdaq.com/symbol/"
-
+for line in html_file.readlines():
+    x = re.search(tickerbaselink, line)
+    if x is not None:
+        print(x.string)
 # fetches the first n valid tickers from the following URL and write tickes in
 # file tickers.txt
 # def save_tickers():
-
-
-
