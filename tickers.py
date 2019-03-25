1c972b66d73999d55068ab0f2c74a4878a719430
Fernanda Rodriguez <fdr16@my.fsu.edu>
2019-03-25 11:28:03 -0400

    can read from url

--- tickers.py
+++ tickers.py
@@ -2,3 +2,19 @@
  a .py file for tickers module. use python3 tickers.py number_of_tickers
  ticker_filename to execute
 """
+
+import requests
+
+link = "http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQrender=download"
+
+f = requests.get(link)
+print(f.text)
+
+tickerbaselink = "https://www.nasdaq.com/symbol/"
+
+# fetches the first n valid tickers from the following URL and write tickes in
+# file tickers.txt
+# def save_tickers():
+
+
+
