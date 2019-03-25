1150204da8e9e4397edcba22fa7f8356a5999551
Guillermo Villegas <44106664+gevillegas@users.noreply.github.com>
2019-03-25 19:35:03 -0400

    Add files via upload

--- tickers.py
+++ tickers.py
@@ -6,35 +6,37 @@ import os.path
 import requests
 import re
 from iex import Stock
-link = "https://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&pagesize=200"
-tickers = []
-d = requests.get(link)
 
-html_file = open('html.txt', 'w')
-html_file.write(d.text)
-html_file = open('html.txt')
-
-tickerbaselink = "https://www.nasdaq.com/symbol/"
-i = 1
-for line in html_file.readlines():
-    x = re.search(tickerbaselink, line)
-    if x is not None:
-    #really ratchet  counter, but it works :)
-        if i is 1:
-            t = os.path.basename(x.string).upper()
-            t = t[:-3]
-            tickers.append(t)
-        if i is 3:
-            i = 0
-        i = i + 1
 
 # fetches the first n valid tickers from the following URL and write tickes in
 # file tickers.txt
 def save_tickers(amountOfTickers):
+    link = "https://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&pagesize=200"
+    tickers = []
+    d = requests.get(link)
+
+    html_file = open('html.txt', 'w')
+    html_file.write(d.text)
+    html_file = open('html.txt')
+
+    tickerbaselink = "https://www.nasdaq.com/symbol/"
+    i = 1
+    for line in html_file.readlines():
+        x = re.search(tickerbaselink, line)
+        if x is not None:
+        #really ratchet  counter, but it works :)
+            if i is 1:
+                t = os.path.basename(x.string).upper()
+                t = t[:-3]
+                tickers.append(t)
+            if i is 3:
+                i = 0
+            i = i + 1
+    
     if amountOfTickers > 150:
         raise IndexError("n passed to save_tickers out of range. n must be <= 150")
     f = open('tickers.txt', 'w')
-    
+
     n = 0
     validTickers = []
     for x in tickers:
