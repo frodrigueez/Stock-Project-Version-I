155f44d2b8088945d272cfbdfc245efe7e0e4d4d
Fernanda Rodriguez <fdr16@my.fsu.edu>
2019-03-25 20:27:35 -0400

    started fetcher.py

--- fetcher.py
+++ fetcher.py
@@ -1,3 +1,27 @@
 """
 to execute: python3 fetcher.py time_lim ticker_filename info_filename
 """
+import os 
+import sys 
+import time
+
+timestarted = time.time
+timetorun = int(sys.argv[1])
+timetoend = int(timetorun + timestarted)
+
+print(timetoend)
+
+# while time.time < timetoend:
+    
+
+# read all tickers from an input file
+f = open(sys.argv[2], 'r')
+for line in f.readlines():
+    print(line, end='')
+
+# this function will update current stock information for ticker passed as arg
+# will write/update in an information file (ex: info.csv)
+def updateStockInfo(ticker):
+    w = open(sys.argv[3], 'w')
+
+
