a8faa4e76086acd8589c90ae834a11e510c1c7c9
Fernanda Rodriguez <fdr16@my.fsu.edu>
2019-03-28 12:20:35 -0400

    need to train data set

--- predictor.py
+++ predictor.py
@@ -8,14 +8,23 @@ import pandas
 import argparse
 import matplotlib.pyplot as plt
 from query import queryStock
+from sklearn.model_selection import train_test_split
+import re
+
 """
 A function that reads data from infofile, selects data for specified ticker,
 & trains machine learning based model to predict value of specified col for
 next t mins
 """
 #def trainData(infofile, ticker, col, t, graphfile):
-# read data from info file, select data for specified data 
-
+# read data from info file
+# queryStock(args.info_filename, "15:03", args.ticker, False)
+# select data for specified ticker
+# train machine learning based model to preduct value of col
+# for next t minutes
+# predict val of col using time column (can split into hrs and 
+# mins) based on historical 
+ 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
@@ -26,9 +35,20 @@ if __name__ == "__main__":
     parser.add_argument("t")
     
     args = parser.parse_args()
-    queryStock(args.info_filename, "15:03", args.ticker, False)
-    # initalizing inital linear regression object 
-    model = LinearRegression()
-    #regressor.fit()
+
+    data = pandas.read_csv(args.info_filename)
+    print(data)
+    #print(df.keys())
+    #print(df.Ticker)
+    #print(df)
+    #tickers = data."Tickers".tolist()
+
+
+    hi =queryStock(args.info_filename, "15:03", args.ticker, False)
+    print(hi)
+# initalizing inital linear regression object 
+#    model = LinearRegression()
+    #model.fit(x,y)
+
 
 
