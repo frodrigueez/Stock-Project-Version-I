1b1377b2a4ed73799f8b49c0a6606a0113846c99
Fernanda Rodriguez <fdr16@my.fsu.edu>
2019-03-28 15:13:45 -0400

    now gets historical data and stores in list

--- predictor.py
+++ predictor.py
@@ -10,13 +10,8 @@ import matplotlib.pyplot as plt
 from query import queryStock
 from sklearn.model_selection import train_test_split
 import re
-
-"""
-A function that reads data from infofile, selects data for specified ticker,
-& trains machine learning based model to predict value of specified col for
-next t mins
-"""
-
+import sys 
+import os
 """
 this function will return a list of all mins for which there are entries
 to help gather historical stock data
@@ -31,39 +26,39 @@ def getAllMins(infofile):
             mins.add(search.group()[:-1])
     return mins
 
+def getHistoricalData(infofile, mins, ticker):
+    hd = []
+    for m in mins:
+        sys.stdout = open(os.devnull, 'w')
+        hd.append(queryStock(infofile, m, ticker, False))
+        sys.stdout = sys.__stdout__
+    return hd
+
 """
 A function that reads data from infofile, selects data for specified ticker,
 & trains machine learning based model to predict value of specified col for
 next t mins
 """
 def trainData(infofile, ticker, col, t, graphfile):
-# read data from info file
-    for i in range( t ):
-        queryStock(args.info_filename, "15:03", args.ticker, False)
-# select data for specified ticker
-# train machine learning based model to preduct value of col
-# for next t minutes
-# predict val of col using time column (can split into hrs and 
-# mins) based on historical 
- 
+    mins = getAllMins(infofile)
+    hdata = getHistoricalData(infofile, mins, ticker)
+    print(hdata)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-
     parser.add_argument("ticker")
     parser.add_argument("info_filename")
     parser.add_argument("graph_filename")
     parser.add_argument("col")
     parser.add_argument("t")
-    
     args = parser.parse_args()
 
     data = pandas.read_csv(args.info_filename)
-    # print(data)
-    getAllMins(str(args.info_filename))
-    #hi =queryStock(args.info_filename, "15:03", args.ticker, False)
-    #print(hi)
+    
+
+    trainData(args.info_filename, args.ticker, args.col, args.t, args.graph_filename)
     # initalizing inital linear regression object 
-    # model = LinearRegression()
+    model = LinearRegression()
     # model.fit(x,y)
 
 
