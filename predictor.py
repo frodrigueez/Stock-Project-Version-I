5cc599ca514fda354e80ae9c164b00adb611b38d
Fernanda Rodriguez <fdr16@my.fsu.edu>
2019-03-27 20:36:49 -0400

    started predictor module

--- predictor.py
+++ predictor.py
@@ -2,3 +2,16 @@
 a .py file for the predictor module. to execute: python3 predictor.py ticker
 info_filename graph_filename col t
 """
+import sklearn.linear_model import LinearRegression
+
+
+"""
+A function that reads data from infofile, selects data for specified ticker, 
+& trains machine learning based model to predict value of specified col for
+next t mins
+"""
+def trainData(infofile, ticker, col, t, graphfile):
+
+
+if __name__ == "__main__":
+
