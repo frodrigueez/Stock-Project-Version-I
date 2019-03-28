8962ad50f7afb7907f806cc50ef58e5e5ca3e026
Fernanda Rodriguez <fdr16@my.fsu.edu>
2019-03-27 20:24:05 -0400

    added flag support. needs queryStock()

--- query.py
+++ query.py
@@ -13,7 +13,9 @@ import argparse
 """
 prints details corresponding to specific time and ticker symbol to terminal
 """
-def queryStock(infofile, time, ticker):
+def queryStock(infofile, time, ticker, verbose):
+    if verbose:
+        print(f"verbose is on")
     with open(infofile) as f:
         for line in f:
             if(time == line[:5]):
@@ -32,19 +34,27 @@ def queryStock(infofile, time, ticker):
                     print("lll")
 
 if __name__ == "__main__":
+    # initalizing all required flags
     parser = argparse.ArgumentParser()
     parser.add_argument("-verbose", help="will print number of rows, columns and name of columns in info file", action="store_true")
-    #parser.add_argument("time", help="HH:MM format", action=)
-    args = parser.parse_args()
+    parser.add_argument("-file", help="info_filename",action="store_true", required=True)
+    parser.add_argument("info_filename")
+    parser.add_argument("-ticker", help="ticker name",action="store_true", required=True)
+    parser.add_argument("tickername")
+    parser.add_argument("-time", help="HH:MM format", action="store_true", required=True)
+    parser.add_argument("giventime")
 
-    if args.verbose:
-        print("verbose is turned on!")
-    else:
-        print("verbose is turned off")
-    #vflag = sys.argv[1]
-        #if vflag == "true":
-        #    print(f"vflag is on")
-        #else:
-        #    print(f"vflag is off")
+    args = parser.parse_args()
+    print(f".info_filename: {type(args.info_filename)} .giventime:{type(args.giventime)}, .tickername:{type(args.tickername)}, .verbose:{type(args.verbose)}")
+    queryStock(args.info_filename, args.giventime, args.tickername, args.verbose)
+
+    #if args.file and args.info_filename and args.ticker and args.tickername and args.time and args.giventime:
+    #    if args.verbose:
+    #        print("verbose is turned on!")
+    #    else:
+    #        print("correct arguments with no verbose")
+    #else:
+    #    print("usage: python3 [-verbose] -file info_filename -ticker ticker -time time")
+
 
     # queryStock(sys.argv[1], sys.argv[2], sys.argv[3])
