bbb21ede7915a220d62b465a64737423a693300f
Guillermo Villegas <44106664+gevillegas@users.noreply.github.com>
2019-03-27 21:40:25 -0400

    Add files via upload

--- predictor.py
+++ predictor.py
@@ -2,16 +2,16 @@
 a .py file for the predictor module. to execute: python3 predictor.py ticker
 info_filename graph_filename col t
 """
-import sklearn.linear_model import LinearRegression
-
-
+from sklearn.linear_model import LinearRegression
+from query import queryStock
+import sys
 """
-A function that reads data from infofile, selects data for specified ticker, 
+A function that reads data from infofile, selects data for specified ticker,
 & trains machine learning based model to predict value of specified col for
 next t mins
 """
-def trainData(infofile, ticker, col, t, graphfile):
-
+#def trainData(infofile, ticker, col, t, graphfile):
 
 if __name__ == "__main__":
-
+    print("hi")
+    queryStock(sys.argv[1], sys.argv[2], sys.argv[3], False)
