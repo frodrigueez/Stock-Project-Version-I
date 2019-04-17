291afd03cc044ec8be91d8e79dbea2c451139b0a
Guillermo Villegas <44106664+gevillegas@users.noreply.github.com>
2019-04-17 10:52:48 -0400

    time limit for fetch_all_data

--- fetcher.py
+++ fetcher.py
@@ -1,96 +1,97 @@
-"""
-to execute: python3 fetcher.py time_lim ticker_filename info_filename
-assumes that proper filenames are passed. (ie: with proper extenstions)
-"""
+import sqlite3
+from tickers import Tickers
+import time
+from datetime import datetime
 from iex import Stock
 import os
 import sys
-import time
-import pandas as pd
-import csv
-from datetime import datetime
-import re
 
-# get all necessary info from argv[]
-time_lim = sys.argv[1]
-ticker_filename = sys.argv[2]
-writefile = sys.argv[3]
-t = {} # each stock entry
-tlist = {} # a dictionary of dictionaries
-keys = ['symbol','low', 'high', 'open', 'close', 'latestPrice', 'latestVolume']
+class Fetcher:
+    """
+    A simple fetcher class
+    """
+    def __init__(self, db, time_limit):
+        """
+        initalizes a fetcher object with database name, and time_limit given db and time_limit. also
+        initalizes object with empty tickers list.
+        given Fetcher object fetcher, you can access these attributes with
+        fetcher.db, fetcher.time_limit and fetcher.tickers
+        """
+        self.db = str(db)
+        self.time_limit = int(time_limit)
+        self.tickers = []
 
-""" this function will update current stock information for ticker passed as arg
-will write/update in an information file (ex: info.csv)
-"""
-def updateStockInfo(ticker):
-    keys = ['symbol','low', 'high', 'open', 'close', 'latestPrice', 'latestVolume']
-    # check for file existance so you don't overwrite header
-    exists = os.path.isfile(writefile)
+    def read_tickers(self):
+        """
+        reads all tickers from 'Ticker.txt' and writes them to Fetcher instance, fetcher, fetcher.tickers where
+        tickers is a list
+        """
+        with open('tickers.txt') as f:
+            for line in f:
+                self.tickers.append(line.strip())
 
-    with open(writefile, mode='a') as csv_file:
-        fieldnames = ['Time', 'Ticker', 'latestPrice', 'latestVolume', 'Close', 'Open', 'low', 'high']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        if exists==False:
-            writer.writeheader()
-            sys.stdout = open(os.devnull, 'w')
-            updatedInfo = Stock(ticker.strip())
-            t = {key:value for key, value in updatedInfo.quote().items() if key in keys}
-            sys.stdout = sys.__stdout__
-            tlist[ticker] = t
-            writer.writerow({'Time': time.strftime("%H:%M"), 'Ticker': ticker.strip(), 'latestPrice':
-                tlist[ticker]['latestPrice'], 'latestVolume': tlist[ticker]['latestVolume'], 'Close': tlist[ticker]['close'],
-                'Open':tlist[ticker]['open'], 'low':tlist[ticker]['low'], 'high':tlist[ticker]['high']})
-        else:
-            # just add the new info every min
-            sys.stdout = open(os.devnull, 'w')
-            updatedInfo = Stock(ticker.strip())
-            t = {key:value for key, value in updatedInfo.quote().items() if key in keys}
-            sys.stdout = sys.__stdout__
-            tlist[ticker] = t
-            writer.writerow({'Time': time.strftime("%H:%M"), 'Ticker': ticker.strip(), 'latestPrice':
-                tlist[ticker]['latestPrice'], 'latestVolume': tlist[ticker]['latestVolume'], 'Close': tlist[ticker]['close'],
-                'Open':tlist[ticker]['open'], 'low':tlist[ticker]['low'], 'high':tlist[ticker]['high']})
+    def create_db(self):
+        """
+        creates database named after Fetcher instance, fetcher, fetcher.db value
+        """
+        conn = sqlite3.connect(self.db)
+        c = conn.cursor()
+        print("Opened database successfully")
+
+        c.execute('''CREATE TABLE IF NOT EXISTS STOCKDATA
+            (TIME TEXT, TICKER TEXT, LOW FLOAT, HIGH FLOAT, OPEN FLOAT, CLOSE FLOAT, PRICE FLOAT, VOLUME INT)''')
+        print("Table created successfully")
+        conn.commit()
+        conn.close()
+
+    def update_stock_info(self,ticker):
+        """
+        updates stock information in database for argument ticker
+        """
+
+        want = ['low', 'high', 'open', 'close', 'latestPrice', 'latestVolume']
+
+        sys.stdout = open(os.devnull, 'w')
+        updatedInfo = Stock(ticker).quote()
+        sys.stdout = sys.__stdout__
+
+        thevalues = {key:value for key, value in updatedInfo.items() if key in want}
+
+        detime = time.strftime("%H:%M")
+
+        conn = sqlite3.connect(self.db)
+        c = conn.cursor()
+
+        print("Opened database successfully")
+        print(f"the values to insert...\n{thevalues}")
+        print(type(thevalues['latestVolume']))
+        c.execute("INSERT INTO STOCKDATA VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (detime, ticker, thevalues['low'], thevalues['high'], thevalues['open'], thevalues['close'], thevalues['latestPrice'], thevalues['latestVolume']))
 
 
-#newtlist = pd.DataFrame(tlist)
-#newtlist.to_csv("tickers.txt")
+        conn.commit()
+        conn.close()
+
+    def fetch_all_data(self):
+        """
+        calls update_stock_info() for all tickers.
+        this will run for specified time period time_lim (in secs)
+        """
+        start_time = time.time()
+        elapsed_time = 0
+        while elapsed < int(self.time_limit):
+            for ticker in self.tickers:
+                self.update_stock_info(ticker)
+            elapsed_time = time.time() - start_time
+            if int(time_lim)-elapsed > 60:
+                time.sleep(60)
+            else:
+                break
 
 
 if __name__ == "__main__":
-    # whole module should be timed
-    # first read tickers from input file
-    f = open(ticker_filename, 'r')
-    start = time.time()
-    #print(f"actual start: {start}")
-    elapsed = 0
-    #print(f"time_lim = {time_lim}")
-    while elapsed < int(time_lim):
-        #print(f"elapsed: {elapsed}")
-        ticker = f.readline()
-        while ticker:
-            if elapsed < int(time_lim):
-                updateStockInfo(ticker)
-                #stock = Stock(ticker.strip())
-                #t = {key:value for key, value in stock.quote().items() if key in keys}
-                #tlist[ticker] = t
-                #print(t)
-                #print(tlist)
-                #print("elapsed:")
-                #print(elapsed)
-                #print("current time:")
-                #print(time.strftime("%H:%M"))
-                elapsed = time.time() - start
-                #time.sleep(60)
-            else:
-                break
-            ticker = f.readline()
-        f = open(ticker_filename, 'r')
-        elapsed = time.time() - start
+    fetcher = Fetcher("stocks_now.db", 60)
+    fetcher.read_tickers()
+    fetcher.create_db()
 
-        if int(time_lim)-elapsed > 60:
-            time.sleep(60)
-        else:
-            #print(f"time lim < 60...{time_lim}")
-            break
 
-    #print(elapsed)
+    fetcher.fetch_all_data()
